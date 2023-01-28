70bd8d7b8302b4c04771a804c219c7e2d5e3a471
Perseverant Determination <persevdt+git@gmail.com>
2023-01-28 22:33:10 +0800

    Add documentation for Encounter

--- script.lua
+++ script.lua
@@ -17,8 +17,9 @@ script.SetVar = function(yourVariableName, value) end
 script.GetVar = function(yourVariableName) end
 
 local function Call(function_name, argument) end
---- Runs `function_name` from within the target script, passing all elements in `arguments`
---- as arguments.
+--- Runs `function_name` from within the target script. If a second argument is
+--- passed, it will be used as the argument for the function. For functions with
+--- multiple parameters, pass a table as the second argument.
 ---
 --- @param function_name string
 --- @param arguments any[]
@@ -26,3 +27,9 @@ local function Call(function_name, argument) end
 --- @overload fun(function_name: string, argument: any)
 --- @overload fun(function_name: string)
 script.Call = function(function_name, arguments) end
+
+--- This is only accessible outside of the encounter file
+--- @class EncounterScript: Script
+--- @field enemies Script[] Filled after the encounter starts
+--- @field Waves Script[] Filled when entering the `DEFENDING` state
+Encounter = {}
